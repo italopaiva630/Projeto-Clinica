9d117a76ce1e41884f28ff175524e66d93f96461
Paiva <italo_paiva@estudante.sesisenai.org.br>
2026-05-06 20:05:25 -0300

    first commit

---
+++ src/main/java/com/senai/Projetods/ProjetodsApplication.java
@@ -0,0 +1,13 @@
+package com.senai.Projetods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetodsApplication.class, args);
+	}
+
+}
